4bcb2fed671881c5ac11733aff5172a6b0f99fad
Michal Domagała <maxisu1708@Interia.pl>
2025-07-26 22:28:10 +0200

    First UI

---
+++ AutoPlannerMiro/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AutoPlannerMiro
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.textBoxTerminal = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTerminal
+            // 
+            this.textBoxTerminal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTerminal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(246)))), ((int)(((byte)(230)))));
+            this.textBoxTerminal.Font = new System.Drawing.Font("Consolas", 12F);
+            this.textBoxTerminal.Location = new System.Drawing.Point(20, 20);
+            this.textBoxTerminal.Multiline = true;
+            this.textBoxTerminal.Name = "textBoxTerminal";
+            this.textBoxTerminal.ReadOnly = true;
+            this.textBoxTerminal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxTerminal.Size = new System.Drawing.Size(918, 280);
+            this.textBoxTerminal.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(246)))), ((int)(((byte)(230)))));
+            this.textBoxInput.Font = new System.Drawing.Font("Consolas", 12F);
+            this.textBoxInput.Location = new System.Drawing.Point(20, 306);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(918, 36);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxLogo.Image = global::AutoPlannerMiro.Properties.Resources.AutoPlannerMiroFull;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(-5, 2);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(954, 570);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 2;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(246)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(950, 570);
+            this.Controls.Add(this.textBoxTerminal);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Name = "Form1";
+            this.Text = "AutoPlanner Miro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTerminal;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+    }
+}
---
+++ AutoPlannerMiro/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoPlannerMiro
+{
+    public partial class Form1 : Form
+    {
+        private int wizardStep = 0;
+        private string[] wizardQuestions = new[]
+        {
+            "How many Forming Stations?",
+            "How many Ready Locations?",
+            "Choose scenario (1, 2, 3):",
+            "Choose layout: H (standard) or K (alternative):",
+            "Type any value to finish wizard..."
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Ustaw focus na textboxInput po starcie
+            this.Load += (s, e) => textBoxInput.Focus();
+
+            // Pierwsza linia terminala i pierwsze pytanie
+            AddLineToTerminal("Welcome to AutoPlanner Miro!");
+            AddLineToTerminal(wizardQuestions[0]);
+        }
+
+        private void AddLineToTerminal(string line)
+        {
+            if (textBoxTerminal.Text.Length > 0)
+                textBoxTerminal.AppendText(Environment.NewLine);
+            textBoxTerminal.AppendText(line);
+            textBoxTerminal.SelectionStart = textBoxTerminal.Text.Length;
+            textBoxTerminal.ScrollToCaret();
+        }
+
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                string input = textBoxInput.Text.Trim();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    AddLineToTerminal("> " + input);
+                    textBoxInput.Clear();
+
+                    // Prosty wizard krok po kroku
+                    wizardStep++;
+                    if (wizardStep < wizardQuestions.Length)
+                    {
+                        AddLineToTerminal(wizardQuestions[wizardStep]);
+                    }
+                    else
+                    {
+                        AddLineToTerminal("Wizard completed! Here you can start planner logic...");
+                        textBoxInput.Enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
