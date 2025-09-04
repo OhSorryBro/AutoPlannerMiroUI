8f3286eabe8186b067420763dcf6f327d9c70083
Michal Domagała <maxisu1708@Interia.pl>
2025-09-04 20:37:55 +0200

    Added first DebugHarness

---
+++ AutoPlannerMiro/DebugHarness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace AutoPlannerMiro
+{
+    internal static class DebugHarness
+    {
+        [Conditional("DEBUG")]
+        public static void RunFileHelpers()
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, "priority_orders.csv");
+            try
+            {
+                var orders = FileHelpers.ReadPriorityOrders(path);
+                Console.WriteLine($"Found {orders.Count} records in: {path}");
+                foreach (var kvp in orders)
+                    Console.WriteLine($"{kvp.Key} => ({kvp.Value.Item1}, {kvp.Value.Item2})");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Read error: " + ex.Message);
+            }
+            Console.WriteLine("Use Enter to finish...");
+            Console.ReadLine();
+        }
+    }
+}
--- AutoPlannerMiro/Program.cs
+++ AutoPlannerMiro/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using AutoPlannerMiro;
 
 namespace AutoPlannerMiro
 {
     internal static class Program
     {
+#if DEBUG
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+#endif
 
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+#if DEBUG
+            ///// ADD here what need to be tested
+            AllocConsole();
+            DebugHarness.RunFileHelpers();
+
+
+
+
+            //Testing area finishes here
+            return; 
+#endif
+            // Release → UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
